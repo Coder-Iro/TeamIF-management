aa29bbeaaf7154883d009d6bfa2efb8be2a0856e
Coder-Iro <Coder-Iro@teamif.io>
2020-10-22 15:38:31 +0900

    Add react-router-dom, react-sidebar

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {Route, Switch} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import Login from "./pages/Login";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const index = (
+    <BrowserRouter basename="/">
+        <Switch>
+            <Route exact path="/" component={Login}/>
+        </Switch>
+    </BrowserRouter>
 );
+
+ReactDOM.render(index, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
