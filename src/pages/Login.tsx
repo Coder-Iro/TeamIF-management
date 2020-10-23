ad6ad98a12b0fb231f87f415d0eacaf4dfb2a3bc
combbm <combbm@naver.com>
2020-10-23 22:37:13 +0900

    feat: Implement login view

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import Label from '../atomics/Label';
+import Input from '../atomics/Input';
 
 const Container = styled.div`
-  display: grid;
-  place-items: center;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputContainer = styled.div`
+  & input {
+    display: block;
+    margin-bottom: 20px;
+  }
+
+  & input:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+const LoginButton = styled.button`
+  width: 250px;
+  height: 36px;
+
+  border: none;
+  border-radius: 3px;
+
+  color: white;
+  background-color: #0048d9;
+
+  margin-top: 1rem;
 `;
 
 const Login: React.FC = () => {
   return (
     <Container>
-      <h2>Login Page</h2>
+      <div>
+        <Title>TeamIF</Title>
+        <InputContainer>
+          <Label htmlFor='id'>아이디</Label>
+          <Input type='text' id='id' placeholder='' />
+
+          <Label htmlFor='password'>비밀번호</Label>
+          <Input type='text' id='password' placeholder='' />
+
+          <LoginButton>로그인</LoginButton>
+        </InputContainer>
+      </div>
     </Container>
   );
 };
