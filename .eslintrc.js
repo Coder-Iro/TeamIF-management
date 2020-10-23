e891970430044cf41e9420326f41f37ab7d47c44
combbm <combbm@naver.com>
2020-10-23 22:00:10 +0900

    style: Apply ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['react-app', 'airbnb', 'eslint-config-prettier'],
+  plugins: ['react-hooks'],
+  rules: {
+    'arrow-body-style': 'off',
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'import/no-unresolved': 'off',
+    'max-len': 'off',
+    'react/jsx-indent': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'linebreak-style': 'off',
+    'import/extensions': 'off'
+  }
+}
