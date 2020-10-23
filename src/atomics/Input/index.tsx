9bf05b4c52bd4447de68d722e440698edbefe003
combbm <combbm@naver.com>
2020-10-23 22:36:49 +0900

    feat: Create atomics about form

---
+++ src/atomics/Input/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 250px;
+  height: 30px;
+
+  border: 1px solid #bdc3c7;
+  border-radius: 3px;
+`;
+
+export default Input;
---
+++ src/atomics/Label/index.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Label = styled.label`
+  color: #a5abad;
+  font-size: 12px;
+`;
+
+export default Label;
