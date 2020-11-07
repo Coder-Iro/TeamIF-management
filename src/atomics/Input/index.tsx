3eb39857fba84761184408815a510ddb6396783a
combbm <combbm@naver.com>
2020-11-07 22:42:01 +0900

    Fix height and padding-left style of Input atomic

--- src/atomics/Input/index.tsx
+++ src/atomics/Input/index.tsx
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   width: 250px;
-  height: 30px;
+  height: 36px;
 
   border: 1px solid #bdc3c7;
   border-radius: 3px;
+
+  padding-left: 0.5rem;
 `;
 
 export default Input;
