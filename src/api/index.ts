1c0874f2156e733edcc6a79ca708c48bf22d53f5
combbm <combbm@naver.com>
2020-11-14 22:55:37 +0900

    Remove withCredentials option

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,6 @@ const host = process.env.REACT_APP_API_HOST || 'http://localhost:8080';
 
 const Api = axios.create({
   baseURL: host,
-  withCredentials: true,
   headers: {
     Authorization: `Basic ${TokenUtil.get()}`
   }
