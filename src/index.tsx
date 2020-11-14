bd9aa38ce21888e03d668da56e296d2f2589ab3b
combbm <combbm@naver.com>
2020-11-14 15:30:45 +0900

    Add ToastContainer tag to use react-toastify

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import * as serviceWorker from './serviceWorker';
 import Login from './pages/Login';
 
 import './styles/global.css';
 
 const index = (
-  <BrowserRouter basename='/'>
-    <Switch>
-      <Route exact path='/' component={Login} />
-    </Switch>
-  </BrowserRouter>
+  <>
+    <BrowserRouter basename='/'>
+      <Switch>
+        <Route exact path='/' component={Login} />
+      </Switch>
+    </BrowserRouter>
+    <ToastContainer />
+  </>
 );
 
 ReactDOM.render(index, document.getElementById('root'));
