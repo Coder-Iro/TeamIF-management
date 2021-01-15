1f141962e9fd6e9a707a4e6dce400a524582ed8f
combbm <combbm@naver.com>
2021-01-15 18:58:22 +0900

    Add icon for sidebar items

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 import React from 'react';
+import {
+  IoBuild,
+  IoCheckmarkDoneSharp,
+  IoLogOutOutline,
+  IoMdPeople
+} from 'react-icons/all';
+import { NavLink } from 'react-router-dom';
 import SideBarItem from './SideBarItem';
 
 const StyledSideBar = styled.div`
@@ -22,6 +29,14 @@ const PageTitle = styled.h1`
   margin: 3rem 0;
 `;
 
+const MenuText = styled.span`
+  margin-left: 4px;
+`;
+
+const activeStyle = {
+  backgroundColor: '#262626'
+};
+
 const SideBar: React.FC = () => {
   return (
     <StyledSideBar>
@@ -30,10 +45,33 @@ const SideBar: React.FC = () => {
         <br />
         Management
       </PageTitle>
-      <SideBarItem>팀원 관리</SideBarItem>
-      <SideBarItem>팀원 모집</SideBarItem>
-      <SideBarItem>프로젝트 관리</SideBarItem>
-      <SideBarItem>로그아웃</SideBarItem>
+
+      <NavLink to='/team/manage' activeStyle={activeStyle}>
+        <SideBarItem>
+          <IoMdPeople size={22} />
+          <MenuText>팀원 관리</MenuText>
+        </SideBarItem>
+      </NavLink>
+
+      <NavLink to='/team/recruit' activeStyle={activeStyle}>
+        <SideBarItem>
+          <IoCheckmarkDoneSharp size={22} />
+          <MenuText>팀원 모집</MenuText>
+        </SideBarItem>
+      </NavLink>
+
+      <NavLink to='/project' activeStyle={activeStyle}>
+        <SideBarItem>
+          <IoBuild size={22} />
+          <MenuText>프로젝트 관리</MenuText>
+        </SideBarItem>
+      </NavLink>
+
+      <SideBarItem>
+        <IoLogOutOutline size={22} />
+        <MenuText>로그아웃</MenuText>
+      </SideBarItem>
+
       {/* TODO: 사이드바에 들어갈 메뉴 추가하기 */}
     </StyledSideBar>
   );
