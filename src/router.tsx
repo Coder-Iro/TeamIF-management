8f25d29015f1d51ad27c7273f22fbacb352a15fc
combbm <combbm@naver.com>
2021-01-15 18:21:21 +0900

    Add MainPage into router

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import MainPage from './pages/MainPage';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter basename='/'>
       <Switch>
+        <Route exact path='/' component={MainPage} />
         <Route exact path='/login' component={LoginPage} />
         <Route exact path='/register' component={RegisterPage} />
       </Switch>
