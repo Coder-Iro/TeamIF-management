74af1a6ae8697f1e7a8066b3e2f07dadd5236ace
combbm <combbm@naver.com>
2021-01-15 14:59:20 +0900

    Remove unnecessary comment

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,4 @@ const index = (
 
 ReactDOM.render(index, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
